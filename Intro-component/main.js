9bf4088402112e8d292c8fc17325ee86819ad63b
code_scrub <jordan_smith95@hotmail.com>
2019-06-09 19:06:47 +0100

    js working
    all fields required, email validation requires an @domain before it will validate.

--- Intro-component/main.js
+++ Intro-component/main.js
@@ -1,10 +1,42 @@
-function formValidation(){
-    const firstName = document.getElementById('fn').Value;
-    if(firstName == ""){
-        element.classList.add('form_invalid');
-        document.getElementById('fn')[0].placeholder='Required field';
-    } else {
-        element.classList.add('form_valid');
+const validateEmail = email => {
+    var re = /.*@.*\..*/;
+    return re.test(String(email).toLowerCase());
+}
 
+const inputIsInvalid = (element, customMessage = null) => {
+  customMessage = customMessage ? customMessage : 'Required field'
+  element.classList.add('form_invalid');
+  element.classList.remove('form_valid');
+  element.setAttribute('placeholder', customMessage);
+}
+
+const inputIsValid = element => {
+  element.classList.add('form_valid');
+  element.classList.remove('form_invalid');
+}
+
+function validateForm(){
+    const elements = document.querySelectorAll('.form_input');
+    for(element of elements) {
+      let value = element.value;
+      
+      if(element.classList.contains('form_input--email')) {
+        if(validateEmail(value)) {
+          inputIsValid(element)
+        } else {
+          inputIsInvalid(element, 'Valid email required')
+        }
+      } else {
+        if(value != ""){
+          inputIsValid(element)    
+        } else {
+          inputIsInvalid(element)    
+        } 
+      }
     }
 }
+
+document.querySelector('.form_body').addEventListener('submit', e => {
+  e.preventDefault()
+  validateForm()
+})
